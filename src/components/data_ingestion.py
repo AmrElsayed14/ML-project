7c9bcee569b7dc558c3b8e740c0d614aaac75c53
Amr Elsayed <engr.amr.mohamed@gmail.com>
2023-08-13 12:42:47 +0200

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,15 +6,21 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
+    '''
+    define paths for the dataset after splitting
+    '''
     train_data_path: str= os.path.join("artifacts", "train.csv")
     test_data_path: str= os.path.join("artifacts", "test.csv")
     raw_data_path: str= os.path.join("artifacts", "data.csv")
 
 class DataIngetsion:
     def __init__(self):
+        # call config class as obj
         self.ingestion_config=DataIngestionConfig()
 
     def intiate_data_ingestion(self):
@@ -24,14 +30,18 @@ class DataIngetsion:
             df = pd.read_csv("Dataset\StudentsPerformance.csv")
             logging.info("Read the dataset as dataframe")
 
+            # make directories if not already exist
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
-
+            # save the raw data as csv file
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train Test Split initiated")
+            # split the data into two sets: train and test set 
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            # save the train and test data as csv files
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)    
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
             logging.info("Ingestion of Data is Completed")
@@ -46,4 +56,7 @@ class DataIngetsion:
 
 if __name__=="__main__":
     obj = DataIngetsion()
-    obj.intiate_data_ingestion()
+    train_data, test_data = obj.intiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
