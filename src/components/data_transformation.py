c93ae6fa04de7c02f2c12072451cfb3c5c17f641
Amr Elsayed <engr.amr.mohamed@gmail.com>
2023-08-14 23:45:21 +0200

    app using Flask

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -27,11 +27,11 @@ class DataTransformation:
         Function responsible for data transformation
         '''
         try:
-            numerical_columns = ["writing score", "reading score"]
-            categorical_columns = ['gender', 'race/ethnicity', 
-                                   'parental level of education', 
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = ['gender', 'race_ethnicity', 
+                                   'parental_level_of_education', 
                                    'lunch', 
-                                   'test preparation course']
+                                   'test_preparation_course']
             
             num_pipline = Pipeline(
                 steps=[
@@ -43,12 +43,12 @@ class DataTransformation:
                 steps=[
                     ("imputer", SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
                     
                 ]
             )
-            logging.info("Numerical columns standard scaling completed")
-         
-            logging.info("Categorical columns encoding completed")
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
             preprocessor = ColumnTransformer(
                 [
                     ("num_pipeline", num_pipline, numerical_columns),
@@ -71,8 +71,8 @@ class DataTransformation:
 
             preprocessor_obj = self.get_data_transformer_object()
 
-            target_column_name = "math score"
-            numerical_columns= ["writing score","reading score"]
+            target_column_name = "math_score"
+            numerical_columns= ["writing_score","reading_score"]
 
             input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df = train_df[target_column_name]
@@ -80,7 +80,9 @@ class DataTransformation:
             input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df = test_df[target_column_name]
 
-            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe"
+                )
 
             input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
@@ -88,16 +90,15 @@ class DataTransformation:
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
-            test_arr = np.c_[
-                input_feature_test_arr, np.array(target_feature_test_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            
             logging.info(f"Saved preprocessing object")
 
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_fie_path,
                 obj=preprocessor_obj
             )
-            return
-            (
+            return(
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_fie_path
